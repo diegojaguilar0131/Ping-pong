7aad0794b87ed0f851319e55052956b3b2e1ae59
diegojaguilar <diegojaguilar0131@gmail.com>
2017-06-09 14:42:20 -0700

    add intial files

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var countUp = function(countTo){
+  var result = [];
+  for(var i = 1; i <= countTo; i++){
+      var index = result.indexOf(i);
+      if(i % 15 === 0) {
+        result.splice(index, 0);
+        result.push("Ping-Pong‼️");
+      }else if(i % 5 === 0) {
+        result.splice(index, 0);
+        result.push("Pong❗️");
+      }else if(i % 3 === 0) {
+        result.splice(index, 0);
+        result.push("Ping❕");
+      }else{
+        result.push(i);
+      }
+  }
+  return result;
+};
+
+$(document).ready(function(){
+  $("#userInput").submit(function(event){
+    $("#output").empty();
+    var countTo = parseInt($("input#countTo").val());
+    var output = countUp(countTo);
+
+    output.forEach(function(item){
+      $("#output").append('<li>' + item + '</li>');
+    });
+    event.preventDefault();
+  });
+});
